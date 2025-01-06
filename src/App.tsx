e741530ba25203bf7784615bd3815cbeae72b25a
Chidambaram <chidambaram.t@siemens.com>
2025-01-06 12:07:25 +0530

    fix(App.tsx): added pointer events to none to absolutly positioned div because it was sitting on top of all other normal elements/page content or btns making the btns unclickable

--- src/App.tsx
+++ src/App.tsx
@@ -138,7 +138,7 @@ function App() {
           </ul>
 
           {/* dark & light hero popup icons */}
-          <div className={clsx("absolute left-1/2 h-[200dvh] top-24 -translate-x-1/2 flex flex-col justify-between transition-all duration-1000 dark:rotate-180",isSunMoonIconVisible?"opacity-100":"opacity-0")} >
+          <div className={clsx("absolute left-1/2 h-[200dvh] top-24 -translate-x-1/2 flex flex-col justify-between transition-all duration-1000 dark:rotate-180 pointer-events-none",isSunMoonIconVisible?"opacity-100":"opacity-0")} >
             <svg className="size-10 text-yellow-400 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
               <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
             </svg>
