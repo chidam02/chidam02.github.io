222234c63f02f4747eb470c7dc0d651ff38931be
Chidambaram <chidambaram.t@siemens.com>
2025-01-07 10:13:05 +0530

    feat(ThemeContext.ts): updated type of createContext

--- src/contexts/themeContext/ThemeContext.ts
+++ src/contexts/themeContext/ThemeContext.ts
@@ -1,6 +1,9 @@
 import { createContext, useContext } from "react";
 
-export const ThemeContext = createContext({ theme: "theme", toggleTheme: (theme: string) => {} });
+export const ThemeContext = createContext<{
+  theme: string;
+  toggleTheme: (theme: string) => void;
+}>({ theme: "theme", toggleTheme: () => {} });
 
 export default function useTheme() {
   return useContext(ThemeContext);
